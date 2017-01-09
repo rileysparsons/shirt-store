b53423c45e930fc5af258da04b0cdd10d8876fa2
Riley Parsons <rparsons@scu.edu>
2017-01-09 11:50:35 -0800

    quick aws test

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -19,33 +19,34 @@ Spree.user_class = "Spree::User"
 
 
 #Amazon S3 Config
+if Rails.env.production?
+	attachment_config = {
 
-attachment_config = {
+	  s3_credentials: {
+	    access_key_id:     "AKIAIZXGXZAVGJ3FMAQQ", #ENV['AWS_ACCESS_KEY_ID'],
+	    secret_access_key: "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"#ENV['AWS_SECRET_ACCESS_KEY'],
+	    bucket:            "shirt-website"#ENV['S3_BUCKET_NAME']
+	  },
 
-  s3_credentials: {
-    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    bucket:            ENV['S3_BUCKET_NAME']
-  },
+	  storage:        :s3,
+	  s3_headers:     { "Cache-Control" => "max-age=31557600" },
+	  s3_protocol:    "https",
+	  bucket:         ENV['S3_BUCKET_NAME'],
+	  url:            ":s3_domain_url",
 
-  storage:        :s3,
-  s3_headers:     { "Cache-Control" => "max-age=31557600" },
-  s3_protocol:    "https",
-  bucket:         ENV['S3_BUCKET_NAME'],
-  url:            ":s3_domain_url",
+	  styles: {
+	      mini:     "48x48>",
+	      small:    "100x100>",
+	      product:  "240x240>",
+	      large:    "600x600>"
+	  },
 
-  styles: {
-      mini:     "48x48>",
-      small:    "100x100>",
-      product:  "240x240>",
-      large:    "600x600>"
-  },
+	  path:           "/:class/:id/:style/:basename.:extension",
+	  default_url:    "/:class/:id/:style/:basename.:extension",
+	  default_style:  "product"
+	}
 
-  path:           "/:class/:id/:style/:basename.:extension",
-  default_url:    "/:class/:id/:style/:basename.:extension",
-  default_style:  "product"
-}
-
-attachment_config.each do |key, value|
-  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+	attachment_config.each do |key, value|
+	  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+	end
 end
